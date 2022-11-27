6140c64a0fe3adf51270841160d363052abe5e14
Sergey Kovalev <dipdeepcode@mail.ru>
2022-11-27 23:55:46 +0300

    Init commit

---
+++ src/main/java/ru/ddc/pse/ParkingSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package ru.ddc.pse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingSearchEngineApplication.class, args);
+    }
+
+}
